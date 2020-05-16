bc660a1e9e0a7635037870e36956f9a0ad572773
LadyDeveloper <anhacorrea@hotmail.com>
2020-05-15 21:06:00 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
@@ -20,6 +22,27 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if(permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if(permission.status !== 'granted'){
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date){
+        const permission = await this.obtainNotificationPermission();
+        if(permission.status === 'granted'){
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
 
     resetForm(){
         this.setState({
@@ -97,11 +120,17 @@ Date: ${this.state.date} `
                           
                             [
                                 {
-                                text: "Cancel",
-                                onPress: () => this.resetForm(),
-                                style: "cancel"
+                                    text: "Cancel",
+                                    onPress: () => this.resetForm(),
+                                    style: "cancel"
                                 },
-                                { text: "OK", onPress: () =>  this.resetForm()}
+                                { 
+                                    text: "OK", 
+                                    onPress: () => { 
+                                        this.presentLocalNotification(this.state.date);
+                                        this.resetForm();
+                                    }
+                                }
                             ],
                             { cancelable: false }
                             )
